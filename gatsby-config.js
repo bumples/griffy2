892c507521fef9f554ce5cb963f528a3bfb7bbcf
Olieb2020 <bumples@aol.com>
2020-10-29 18:21:46 -0400

    master com

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,43 +1,52 @@
- module.exports = {
+ require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+module.exports = {
   siteMetadata: {
-    title: `Bumples`,
-    description: `Books for kids 4-10.`,
-    author: `@gatsbyjs`
+    title: `Gatsby Default Starter`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `@gatsbyjs`,
   },
   plugins: [
-      "gatsby-plugin-react-helmet",
-    {
-      resolve: "gatsby-source-contentful",
-      options: {
-          spaceId: `dawlrnrnd2gl `,
-        // Learn about environment variables: https://gatsby.app/env-vars
-        accessToken: `36XlVTeALWMZTfLF8kIPknL1Zn89v2Z0gTGYOHkAVyc`
-      },
-    },
+    `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`
-      }
-    } 
+        path: `${__dirname}/src/images`,
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        // Learn about environment variables: https://gatsby.app/env-vars
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
+    // {
+    //   resolve: "gatsby-plugin-snipcart",
+    //   options: {
+    //     apiKey: process.env.SNIPCART_APIKEY,
+    //     autopop: true,
+    //   },
+    // },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: ` 663399`,
-        theme_color: ` 663399`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
         display: `minimal-ui`,
-        
-      }
-    } 
+        icon: `src/images/logo.svg`, // This path is relative to the root of the site.
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
-  ]
+  ],
 };
-
