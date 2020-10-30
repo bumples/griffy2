b1eb624df66f4ee32bd8dc2b9492fb83f63f7c0f
Olieb2020 <bumples@aol.com>
2020-10-30 15:33:22 -0400

    no about info

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,18 +2,16 @@ import React from "react"
 import { Link,graphql } from "gatsby"
 
 import Layout from "../components/layout"
-import SEO from "../components/seo"
 import BackgroundSection from "../components/Globals/BackgroundSection";
-import Info from "../components/Home/Info";
- 
+
 const IndexPage = ({data}) => (
   <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    
     <BackgroundSection
       img={data.img.childImageSharp.fluid}
       title="bumples"
     />
-      <Info />
+      
         </Layout>
 );
 
