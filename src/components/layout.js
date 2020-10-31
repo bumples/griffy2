95542eac8a367daafe5dd0fbdb197706d3698dfb
Olieb2020 <bumples@aol.com>
2020-10-31 18:47:50 -0400

    addcomponents

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,19 +5,48 @@ import { StaticQuery, graphql } from "gatsby";
 import "./bootstrap.min.css";
 import "./layout.css";
 
-import Navbar from "./Globals/Navbar";
-import Footer from "./Globals/Footer";
+import Header from './header'
+import './layout.css'
+
+const getSiteMetadata = graphql`
+  {
+    site {
+      siteMetadata {
+        title
+        author
+        createdAt
+      }
+    }
+  }
+`
+
 const Layout = ({ children }) => (
-  <>
-    <Navbar />
-    {children}
-    <Footer />
-  </>
-);
+  <StaticQuery
+    query={getSiteMetadata}
+    render={data => (
+      <>
+        <Header siteTitle={data.site.siteMetadata.title} />
+        <div
+          style={{
+            margin: `0 auto`,
+            maxWidth: 960,
+            padding: `0px 1.0875rem 1.45rem`,
+            paddingTop: 0,
+          }}
+        >
+          {children}
+          <footer>
+            Built by {data.site.siteMetadata.author}, Created{' '}
+            {data.site.siteMetadata.createdAt}
+          </footer>
+        </div>
+      </>
+    )}
+  />
+)
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired
-};
+  children: PropTypes.node.isRequired,
+}
 
-export default Layout;
- 
+export default Layout 
